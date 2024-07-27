cb7e53fccf76ddd20e83dbf3eb8e4eb624cf1f6e
devjmv <javier161890@gmail.com>
2024-07-27 22:43:48 +0200

    init

---
+++ src/test/java/com/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package com.fizzbuzz;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void test_fizzbuzz_example() {
+        assertTrue(true);
+    }
+
+}
